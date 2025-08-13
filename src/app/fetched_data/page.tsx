bdcbc194ec8c5ea5dd792c942a4ae69ea4676f85
zad_data_uro <mikolaj.babicz@microsoft.wsei.edu.pl>
2025-08-13 07:57:56 +0200

    commit_fetched_changed

--- src/app/fetched_data/page.tsx
+++ src/app/fetched_data/page.tsx
@@ -1,16 +1,10 @@
-import HomePage from "../page";
+import HomeClient from "@/components/home-client";
+import { Employee } from "@/types/employee";
 
-
-function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export default async function FetchedData() {
-    // Symulacja wolnego serwera (2 sekundy)
-    await delay(2000);
-
-    const res = await fetch("http://localhost:3000/users.json", { cache: "no-store" });
-    const data = await res.json();
-
-    return <HomePage initialEmployees={data} />;
+export default async function Page() {
+	const res = await fetch("http://localhost:3000/users.json", {
+		cache: "no-store",
+	});
+	const data: Employee[] = await res.json();
+	return <HomeClient initialEmployees={data} />;
 }
