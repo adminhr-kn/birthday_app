89e16a116a3e0c9d94ae89afa8f851e25e3b2014
zad_data_uro <mikolaj.babicz@microsoft.wsei.edu.pl>
2025-08-20 05:07:14 +0200

    [feat]- avatars added, responsive [fix]- responsive on desktop (improvements), date filtering (better)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,11 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
+		// body needs smth to refer, h-full to be precise
 		<html lang="en" suppressHydrationWarning className="h-full">
 			<body
+				// body always is 100% height so the same as html, min-height 100vh always at least all the screen, if more content it grows
+				// flex so they become a flex container vertically children of body are going one after another, they can share space with flex-1 etc
 				className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground h-full min-h-screen flex flex-col`}>
 				<ThemeProvider
 					// attribute tells to save the theme in the class attribute (document.documentElement) which is <html> element!
@@ -40,14 +43,13 @@ export default function RootLayout({
 					<div className="ml-[24px]">
 						<PoppingImportantDates />
 					</div>
+					{/* flex-1 says take all the space that is left (navbar has his own height and popping also, ex if navbar and poppoing are 100px bith and the screen is 900px children will take 800px), fkex col yeah also vertical. min-h-0 means you can cut your height given by the flexbox  */}
 					<div className="flex-1 flex flex-col min-h-0">{children}</div>
 				</ThemeProvider>
 			</body>
 		</html>
 	);
 }
-
-
 
 /* REFACTOR IDEA
 
