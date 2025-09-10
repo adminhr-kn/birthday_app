01680fcaa9f0ec2129b9ec616df41969cfc82273
mikowhy33 <koding252@gmail.com>
2025-09-10 05:03:51 +0200

    [fix]-now apparently working?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,19 @@ import HomeClient from "@/components/home-client";
 import { Employee } from "@/types/employee";
 
 export default async function Page() {
-	const res = await fetch(
+	// const res = await fetch(
 
-		"https://birthday-app-chi-indol.vercel.app/users.json",
-		{ cache: "no-store" }
-	);
+	// 	"https://birthday-app-chi-indol.vercel.app/users.json",
+	// 	{ cache: "no-store" }
+	// );
+	// "https://birthday-app-chi-indol.vercel.app/api/employees",
 	const apiRes = await fetch(
 		"https://birthday-app-chi-indol.vercel.app/api/employees",
-
 		{
 			cache: "no-store",
 		}
 	);
 	const newData: any[] = await apiRes.json();
-	const data: Employee[] = await res.json();
+	// const data: Employee[] = await res.json();
 	return <HomeClient newData={newData} />;
 }
